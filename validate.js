c7e28cd4d44010c1a6fd0919c602ef576fe429b0
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2020-03-26 14:27:36 +0000

    Validate label descriptions
    If a label description exceeds 100 characters, the call to the GitHub API to patch the label will fail, which means github-label-sync will fail with the error:
    
    GitHub Error:
    PATCH /repos/alphagov/design-system-team-internal/labels/effort%3A%20days
    422: Validation Failed
    
    Validating the label descriptions first will catch this when doing a dry-run.

---
+++ validate.js
@@ -0,0 +1,14 @@
+const labels = require('./labels.js')
+let errors = []
+
+labels.forEach(label => {
+  if (label.description && label.description.length > 100) {
+    errors.push(`- Description for "${label.name}" exceeds 100 characters`);
+  }
+})
+
+if (errors.length) {
+  console.log("Validation failed:")
+  console.log(errors.join("\n"));
+  process.exit(1);
+}
